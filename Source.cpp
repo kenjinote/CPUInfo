da5a5268fd47dc8986220d720a8506b2dd102f6e
kenji <kenji256@gmail.com>
2018-02-15 18:29:28 +0900

    initial commit

---
+++ Source.cpp
@@ -0,0 +1,80 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <windows.h>
+#include <intrin.h>
+
+TCHAR szClassName[] = TEXT("Window");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hEdit;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hEdit = CreateWindow(TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL | ES_READONLY, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		{
+			char CPUBrandString[0x40];
+			int CPUInfo[4] = { -1 };
+			memset(CPUBrandString, 0, sizeof(CPUBrandString));
+			__cpuid(CPUInfo, 0x80000000);
+			if (CPUInfo[0] >= 0x80000004)
+			{
+				__cpuid(CPUInfo, 0x80000002);
+				memcpy(CPUBrandString, CPUInfo, sizeof(CPUInfo));
+				__cpuid(CPUInfo, 0x80000003);
+				memcpy(CPUBrandString + 16, CPUInfo, sizeof(CPUInfo));
+				__cpuid(CPUInfo, 0x80000004);
+				memcpy(CPUBrandString + 32, CPUInfo, sizeof(CPUInfo));
+				SetWindowTextA(hEdit, CPUBrandString);
+			}
+		}
+		break;
+	case WM_SIZE:
+		MoveWindow(hEdit, 0, 0, LOWORD(lParam), HIWORD(lParam), TRUE);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("CPU 名を取得"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
